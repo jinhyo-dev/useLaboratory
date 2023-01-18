96b3d9cd73bb3fcbcc824c222db8e0cece24f9e9
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-01-18 18:44:53 +0900

    feat : listform edit

--- src/components/ListForm.tsx
+++ src/components/ListForm.tsx
@@ -4,21 +4,40 @@ import "../styles/listForm.css";
 const ListForm = (props: any) => {
   return (
     <div className='listForm-container'>
-        <div className='list-title'>
-            <span>랩실 </span>
-            <span>대표이름 </span>
-            <span>학년 반 번호 </span>
-            <span>사유 </span>
-            <span>확인</span>
-        </div>
-        <div>
-            <span>{props.lab}</span>
-            <span>{props.name}</span>
-            <span>{props.schoolNum}</span>
-            <span>{props.reason}</span>
-            <input type="submit" name="accept" className='accept-btn' value="확인" />
-            <input type="submit" name="reject" className='reject-btn' value="취소" />
-        </div>
+      <div className='list-title'>
+        <span>랩실 </span>
+        <span>대표이름 </span>
+        <span>학년 반 번호 </span>
+        <span>확인</span>
+      </div>
+      <div className='list-item'>
+        <span>{props.lab}LAB 1</span>
+        <span>{props.name}박민규</span>
+        <span>{props.schoolNum}2209</span>
+        <input type="submit" name="accept" className='accept-btn' value="승인" />
+        <input type="submit" name="reject" className='reject-btn' value="취소" />
+      </div>
+      <div className='list-item'>
+        <span>{props.lab}LAB 1</span>
+        <span>{props.name}박민규</span>
+        <span>{props.schoolNum}2209</span>
+        <input type="submit" name="accept" className='accept-btn' value="승인" />
+        <input type="submit" name="reject" className='reject-btn' value="취소" />
+      </div>
+      <div className='list-item'>
+        <span>{props.lab}LAB 1</span>
+        <span>{props.name}박민규</span>
+        <span>{props.schoolNum}2209</span>
+        <input type="submit" name="accept" className='accept-btn' value="승인" />
+        <input type="submit" name="reject" className='reject-btn' value="취소" />
+      </div>
+      <div className='list-item'>
+        <span>{props.lab}LAB 1</span>
+        <span>{props.name}박민규</span>
+        <span>{props.schoolNum}2209</span>
+        <input type="submit" name="accept" className='accept-btn' value="승인" />
+        <input type="submit" name="reject" className='reject-btn' value="취소" />
+      </div>
     </div>
   )
 }
--- src/components/SignForm.tsx
+++ src/components/SignForm.tsx
@@ -9,13 +9,13 @@ const SignForm = () => {
         <p className="select-lab">랩실 선택</p>
         <select name='lab-select' className="lab-select">
           <option selected>랩실 선택</option>
-          <option value="lab1">Lab 1</option>
-          <option value="lab2">Lab 2</option>
-          <option value="lab3">Lab 3</option>
-          <option value="lab4">Lab 4</option>
-          <option value="lab5">Lab 5</option>
-          <option value="lab6">Lab 6</option>
-          <option value="lab7">Lab 7</option>
+          <option value="lab1">LAB 1</option>
+          <option value="lab2">LAB 2</option>
+          <option value="lab3">LAB 3</option>
+          <option value="lab4">LAB 4</option>
+          <option value="lab5">LAB 5</option>
+          <option value="lab6">LAB 6</option>
+          <option value="lab7">LAB 7</option>
         </select>
       </div>
       <div className="lab-div">
@@ -25,10 +25,6 @@ const SignForm = () => {
       <div className="lab-div">
         <p className="school-lab">학년 반 번호</p>
         <input type="text" name="schoolNum" className="schoolNum-input" placeholder='예)2206' />
-      </div>
-      <div className="lab-div">
-        <p className="reason-lab">사유</p>
-        <input type="text" name="reason" className="reason-input" placeholder='예)친구들과 소통하면서 공부를 하기 위해'/>
       </div>
       <input
         type="submit"
