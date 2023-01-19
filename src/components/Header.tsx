08c16e88a240adeed31337fdf237e81cda78d78a
jinhyo-dev <jinhyo0850@gmail.com>
2023-01-19 22:26:09 +0900

    feat: Sidebar handle function add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,52 +6,65 @@ import { Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [isBarOpen, setIsBarOpen] = useState<boolean>(true)
+
+  const handleOpen = () => {
+    setIsBarOpen(!isBarOpen)
+  }
+
   return (
     <div className="header-container">
-      <div className="sidebar">
-        <AiOutlineMenu className="AiOutlineMenu" />
+      <div className={isBarOpen ? "open-sidebar" : "not-open-sidebar"}>
+        {
+          isBarOpen ? (
+              <div className={'sidebar'}>
+                <div className='sidebar-container'>
+                  <AiOutlineClose className='AiOutlineClose' onClick={handleOpen}/>
 
-        <div className='sidebar-container'>
-          <AiOutlineClose className='AiOutlineClose' />
+                  <div className='menu-list'>
+                    <Link to="/sign">
+                      <input
+                        type="submit"
+                        value="랩실 신청"
+                        className='sign-btn'
+                      />
+                    </Link>
 
-          <div className='menu-list'>
-            <Link to="/sign">
-              <input
-                type="submit"
-                value="랩실 신청"
-                className='sign-btn'
-              />
-            </Link>
+                    <Link to="/list">
+                      <input
+                        type="submit"
+                        value="랩실 신청자목록"
+                        className='list-btn'
+                      />
+                    </Link>
 
-            <Link to="/list">
-              <input
-                type="submit"
-                value="랩실 신청자목록"
-                className='list-btn'
-              />
-            </Link>
-
-            <Link to="/admin">
-              <input
-                type="submit"
-                value="관리자"
-                className='admin'
-              />
-            </Link>
-          </div>
-        </div>
+                    <Link to="/admin">
+                      <input
+                        type="submit"
+                        value="관리자"
+                        className='admin'
+                      />
+                    </Link>
+                  </div>
+                </div>
+              </div>)
+            :
+            (
+             <button onClick={handleOpen}>
+               오픈
+             </button>
+            )
+        }
       </div>
 
-      <div style={{ textAlign: 'center' }}>
+      <div style={{textAlign: 'center'}}>
         <Link to="/">
-          <img src={Logo} alt="logo" className="logo" />
+          <img src={Logo} alt="logo" className="logo"/>
         </Link>
       </div>
 
-
-
     </div>
-  );
-};
+  )
+}
 
 export default Header;
