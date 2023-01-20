a133e0609dcf69ac3ae780536fa6699cd04b164d
jinhyo-dev <jinhyo0850@gmail.com>
2023-01-21 03:59:00 +0900

    style: Sidebar open and close animation add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import "../styles/header.css";
-import "../styles/sidebar.css";
-import Logo from "../img/logo.png";
-import { Link } from "react-router-dom";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import React, { useState } from "react"
+import "../styles/header.css"
+import "../styles/sidebar.css"
+import Logo from "../img/logo.png"
+import { Link } from "react-router-dom"
+import { AiOutlineMenu } from "react-icons/ai"
+import { RiCloseFill } from 'react-icons/ri'
 
 const Header = () => {
-  const [isBarOpen, setIsBarOpen] = useState<boolean>(true)
+  const [isBarOpen, setIsBarOpen] = useState<boolean>(false)
 
   const handleOpen = () => {
     setIsBarOpen(!isBarOpen)
@@ -14,54 +15,51 @@ const Header = () => {
 
   return (
     <div className="header-container">
-      <div className={isBarOpen ? "not-open-sidebar" : "open-sidebar"}>
+      <div>
         {
-          isBarOpen ? (
+          !isBarOpen && (
             <button onClick={handleOpen} className="AiOutlineMenu">
-              <AiOutlineMenu />
+              <AiOutlineMenu/>
             </button>
           )
-            :
-            (
-              <div className={'sidebar'}>
-                <div className='sidebar-container'>
-                  <AiOutlineClose className='AiOutlineClose' onClick={handleOpen} />
-
-                  <div className='menu-list'>
-                    <Link to="/sign">
-                      <input
-                        type="submit"
-                        value="랩실 신청"
-                        className='sign-btn'
-                      />
-                    </Link>
-
-                    <Link to="/list">
-                      <input
-                        type="submit"
-                        value="랩실 신청자목록"
-                        className='list-btn'
-                      />
-                    </Link>
-
-                    <Link to="/admin">
-                      <input
-                        type="submit"
-                        value="관리자"
-                        className='admin'
-                      />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-
-            )
         }
+
+        <div className={isBarOpen ? "sidebar" : "not-open-sidebar"} id='sidebar'>
+          <div className='sidebar-container'>
+            <RiCloseFill className='RiCloseFill' onClick={handleOpen}/>
+
+            <div className='menu-list'>
+              <Link to="/sign">
+                <input
+                  type="submit"
+                  value="랩실 신청"
+                  className='sign-btn'
+                />
+              </Link>
+
+              <Link to="/list">
+                <input
+                  type="submit"
+                  value="랩실 신청자목록"
+                  className='list-btn'
+                />
+              </Link>
+
+              <Link to="/admin">
+                <input
+                  type="submit"
+                  value="관리자"
+                  className='admin'
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div style={{ textAlign: 'center' }}>
+      <div style={{textAlign: 'center'}}>
         <Link to="/">
-          <img src={Logo} alt="logo" className="logo" />
+          <img src={Logo} alt="logo" className="logo"/>
         </Link>
       </div>
 
