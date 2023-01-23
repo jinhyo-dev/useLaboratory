631990ed9a198732158b36dbab075dafe610f638
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-01-24 01:02:41 +0900

    feat: 대여기간 추가

--- src/components/SignForm.tsx
+++ src/components/SignForm.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import "../styles/signForm.css"
+import React, { useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import "../styles/signForm.css";
 
 const SignForm = () => {
+
+  const date = new Date()
+  const month = date.getMonth()
+  const day = date.getDate()
+  const year = date.getFullYear()
+  const [startDate, setStartDate] = useState<Date>(month > 2 ? new Date(year, 2, 1) : new Date(year, month, day - 7));
+  const [endDate, setEndDate] = useState<Date>(date);
+
   return (
     <div className='signForm-container'>
       <h2 className="sign-text-h2">랩실 신청</h2>
@@ -25,6 +35,29 @@ const SignForm = () => {
       <div className="lab-div">
         <p className="school-lab">학년 반 번호</p>
         <input type="text" name="schoolNum" className="schoolNum-input" placeholder='예)2206' />
+      </div>
+      <div className="lab-div">
+        <p className="date-lab">대여 시작기간</p>
+          <ReactDatePicker
+            dateFormat={'yyyy-MM-dd'}
+            selected={startDate}
+            onChange={(date: Date) => setStartDate(date)}
+            startDate={startDate}
+            endDate={endDate}
+            maxDate={endDate}
+          />
+      </div>
+      <div className='lab-div'>
+        <p className='date-lab'>대여 종료기간</p>
+           <ReactDatePicker
+            dateFormat={'yyyy-MM-dd'}
+            selected={endDate}
+            onChange={(date: Date) => setEndDate(date)}
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            maxDate={date}
+          />
       </div>
       <div className="lab-div">
         <p className="pw-lab">비밀번호</p>
